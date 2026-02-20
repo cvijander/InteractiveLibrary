d934abf3c2c5fbdd54c8735395d1865d1af9e7fb
Cvijander <cvija85@gmail.com>
2026-02-20 08:15:21 +0100

    "Sub meni"

--- AnthonyRobbins.AwakenTheGiantWthin.Console/Program.cs
+++ AnthonyRobbins.AwakenTheGiantWthin.Console/Program.cs
@@ -9,17 +9,83 @@
         Console.WriteLine("                              - Tony Robbins");
         Console.WriteLine("===============================================================");
         Console.ResetColor();
+        Console.WriteLine();
         Console.WriteLine("Hello people to fabolous book of  Tony Robbins");
-        Console.WriteLine("--   Awaken the giant within  -- ");
+        Console.WriteLine();
+        Console.WriteLine("         --   Awaken the giant within  -- ");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press ENTER to start");
+        Console.WriteLine();
+        Console.ReadLine();
         
+
+        DisplayMenu();
+
     }
 
     private static void DisplayMenu()
     {
-        Console.WriteLine("Please select an option:");
-        Console.WriteLine("1. Read a chapter");
-        Console.WriteLine("2. Take notes");
-        Console.WriteLine("3. ");
-        Console.WriteLine("3. Exit");
+        Console.Clear();
+        Console.WriteLine("==== GLAVNI MENU: PROBUDITE DIVA U SEBI ===");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("1. Oslobodite svoju moc");
+        Console.WriteLine("2. Preuzimanje kontrole - glavni sistem ovladavanja");
+        Console.WriteLine("3. Sedam dana za oblikovanje vaseg zivota");
+        Console.WriteLine("4. Pouka o sudbini");
+        Console.WriteLine("5. Definicije i citati");
+        Console.WriteLine("6. Ljudi i primeri iz knjige");
+        Console.WriteLine("7  Vase beleske");
+        Console.WriteLine("0  Izlaz iz programa" );
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("Unesite opciju: ");
+        string userInput = Console.ReadLine();
+
+        bool userOption = true;
+
+
+        userOption = HandleUserInput(userInput);
+        while (!userOption)
+        {
+            DisplayMenu();
+        }
+
+
+
+    }
+
+    private static bool HandleUserInput(string input)
+    {
+        if (input == "1")
+        {
+            FreeYourPower();
+            return true;
+        }
+        else if (input == "2")
+        {
+            TakeControl();
+            return true;
+        }
+        else 
+        {
+            Console.WriteLine("Molimo unesite neku od ponudjenih opcija.");
+            Console.ReadLine();
+            return false;
+        }
+    }
+
+    private static void FreeYourPower()
+    {
+        Console.Clear();
+        Console.WriteLine("1");
+    }
+    private static void TakeControl()
+    {
+        Console.Clear();
+        Console.WriteLine("2");
     }
 }
