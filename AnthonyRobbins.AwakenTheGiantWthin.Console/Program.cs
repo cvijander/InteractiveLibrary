04c1110554927b45a8fa76dcbe2962d9380dcebd
Cvijander <cvija85@gmail.com>
2026-02-20 07:19:01 +0100

    "Main menu - started"

---
+++ AnthonyRobbins.AwakenTheGiantWthin.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("===============================================================");
+        Console.WriteLine(" \"Posvećeno neograničenoj moći koja leži uspavana u Vama.\"");
+        Console.WriteLine("  \"Ne dozvolite joj da i dalje drema. \"");
+        Console.WriteLine("                              - Tony Robbins");
+        Console.WriteLine("===============================================================");
+        Console.ResetColor();
+        Console.WriteLine("Hello people to fabolous book of  Tony Robbins");
+        Console.WriteLine("--   Awaken the giant within  -- ");
+        
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("Please select an option:");
+        Console.WriteLine("1. Read a chapter");
+        Console.WriteLine("2. Take notes");
+        Console.WriteLine("3. ");
+        Console.WriteLine("3. Exit");
+    }
+}
