528018cc62460978b58c9818781328c89ba54597
Cvijander <cvija85@gmail.com>
2026-04-04 08:52:40 +0200

    try catch za ucitvanje i snimanje za korisnicke beleske je podesen

--- AnthonyRobbins.AwakenTheGiantWthin.Console/Menus/NotesMenu.cs
+++ AnthonyRobbins.AwakenTheGiantWthin.Console/Menus/NotesMenu.cs
@@ -14,8 +14,9 @@ namespace AnthonyRobbins.AwakenTheGiantWthin.ConsoleApp.Menus
             Console.WriteLine("==== OSMI DEO: VASE BELESKE  ====");
             Console.WriteLine();
             Console.WriteLine();
+            ReadAllNotes();
             Console.WriteLine();
-            Console.WriteLine("1. Beleske ");
+            Console.WriteLine("1. Unesi novu belesku ");
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("0. Povratak u glavni meni");
@@ -42,49 +43,12 @@ namespace AnthonyRobbins.AwakenTheGiantWthin.ConsoleApp.Menus
 
         private static bool HandleDisplayNotes(string input)
         {
-            Console.WriteLine("Do sada unete beleske ");
-            try
-            {
-                string content = File.ReadAllText(@"D:\Dev\csharp\InteractiveLibrary\AnthonyRobbins.AwakenTheGiantWthin.Console\KorisnickeBeleske.txt");
-                Console.WriteLine(content);
-            }
-
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
-                Console.WriteLine("Proverite samo da li je naziv beleski lepo napisan \"KorisnickeBeleske.txt\"");
-                Console.WriteLine(ex.Message);
-            }
-
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
-                Console.WriteLine(@"Proverite samo da li direktorijum D:\\Dev\\csharp\\InteractiveLibrary\\AnthonyRobbins.AwakenTheGiantWthin.Console postoji");
-                Console.WriteLine(ex.Message);
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("***** zatvaramo ovaj deo");
-                Console.ReadLine();
-            }
-
+            
 
             switch (input)
             {
                 case "1":
-                    Console.Clear();
-                    Console.WriteLine("Unesite vase beleske, ovde, te iste beleske videce u text fajlu nazvan \"KorisnickeBeleske.txt\"");
-                    string beleske = "";
-                    string userInput = Console.ReadLine();
-                    beleske = beleske + "\n" + userInput;
-
-                    System.IO.File.AppendAllText(@"D:\Dev\csharp\InteractiveLibrary\AnthonyRobbins.AwakenTheGiantWthin.Console\KorisnickeBeleske.txt", beleske);
+                    SaveAllNotes();
                     return true;
 
                 case "0":
@@ -99,6 +63,94 @@ namespace AnthonyRobbins.AwakenTheGiantWthin.ConsoleApp.Menus
                     return true;
             }
 
+        }
+
+        private static void ReadAllNotes()
+        {
+            DistanceNotes();
+            try
+            {
+                string content = File.ReadAllText(@"D:\Dev\csharp\InteractiveLibrary\AnthonyRobbins.AwakenTheGiantWthin.Console\KorisnickeBeleske.txt");
+                Console.WriteLine(content);
+            }
+
+            catch (Exception ex)
+            {
+                CentralizedError(ex);                
+            }           
+            finally
+            {
+                DistanceNotes();
+            }
+
+        }
+
+        private static void SaveAllNotes()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("Unesite vase beleske, ovde, te iste beleske videce u text fajlu nazvan \"KorisnickeBeleske.txt\"");
+                string beleske = "";
+                string userInput = Console.ReadLine();
+                beleske = beleske + "\n" + userInput;
+                System.IO.File.AppendAllText(@"DD:\Dev\csharp\InteractiveLibrary\AnthonyRobbins.AwakenTheGiantWthin.Console\KorisnickeBeleske.txt", beleske);
+            }
+
+            catch (Exception ex)
+            {
+                CentralizedError(ex);
+            }            
+
+           
+        }
+
+        private static void DistanceNotes()
+        {
+            Console.WriteLine();
+            Console.WriteLine("**********************************************************************************************************************************");
+            Console.WriteLine();
+        }
+
+        private static void CentralizedError(Exception ex )
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            if (ex is FileNotFoundException)
+            {
+                
+                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
+                Console.WriteLine("Proverite samo da li je naziv beleski lepo napisan \"KorisnickeBeleske.txt\"");
+                Console.WriteLine();
+                Console.WriteLine(ex.Message);
+                
+            }
+
+            else if (ex is DirectoryNotFoundException )
+            {
+               
+                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
+                Console.WriteLine(@"Proverite samo da li direktorijum D:\\Dev\\csharp\\InteractiveLibrary\\AnthonyRobbins.AwakenTheGiantWthin.Console postoji");
+                Console.WriteLine();
+                Console.WriteLine(ex.Message);
+                
+            }
+            else
+            {
+                
+                Console.WriteLine("Nazalost, desio se problem sa aplikacijom");
+                Console.WriteLine();
+                Console.WriteLine(ex.Message);
+                
+            }
+
+            Console.ResetColor();
+
+
+            Console.WriteLine();
+            Console.WriteLine("***********************");
+            Console.WriteLine(" Pritisnite ENTER da potvrdite i da se vratite u meni ");
+            Console.ReadLine();
         }
 
         #endregion
